92738b8d5283bd64ef828778c91ecc3ba79ea2fc
JARLmo <marcusmendez@jarlmo.lan>
2024-01-06 02:00:39 -0600

    future browser router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+function App() {
+  return (
+    <BrowserRouter basename="/app">
+      <Routes>
+        <Route path="/bio" /> {App.js}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+function App() {
+  return (
+    <BrowserRouter future={{ v7_startTransition: true }}>
+      <Routes>{/*...*/}</Routes>
+    </BrowserRouter>
+  );
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>{/* The rest of your app goes here */}</BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
